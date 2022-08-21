1f490aa9aa08a1c9cb1d4d9da35cb7b28da03b4d
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-08-21 15:45:20 +0530

    changes in fav-icons and add title using react-helmet

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ import Portfolio from './component/Portfolio';
 import Experience from './component/Experience';
 import Contact from './component/Contact';
 
+import {Helmet} from 'react-helmet';
+
 export default function App(){
   return(
     <>
+     <Helmet>
+        <title>Portfolio</title>
+    </Helmet>
       <Navbar />
       <Home/>
       <About/>
